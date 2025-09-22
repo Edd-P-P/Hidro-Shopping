bbf2c612752cf18598a93f559f7afe1e99076054
Edd-P-P <lalos7617@gmail.com>
2025-09-22 13:10:36 -0600

    Cambio de color del carrito

--- details.php
+++ details.php
@@ -125,7 +125,7 @@ if($id == '' || $token == ''){
                 <a href="#"><i class="fas fa-user"></i></a>
                 <a href="#" class="icon-wrapper">
                     <i class="fas fa-shopping-cart"></i>
-                    <span id="num_cart" class="badge bg-secondary"></span>
+                    <span id="num_cart" class="cart-count"></span>
                 </a>
             </div>
         </div>
