3469f7ea01dd8875baaa9e8fd75ac225723b3366
Edd-P-P <lalos7617@gmail.com>
2025-11-20 09:10:46 -0600

    Imagenes C-80

--- Imagenes/Renombra_imagenes.py
+++ Imagenes/Renombra_imagenes.py
@@ -31,7 +31,7 @@ def renombrar_imagenes(ruta_carpeta, nombre_base=""):
         archivos.sort()
 
     # Renombrar archivos
-    contador = 17
+    contador = 59
     for archivo in archivos:
         extension = os.path.splitext(archivo)[1]
         
